6387194884b0e0bd02999356409acf4249d54c12
marquizq <michaelmallya@gmail.com>
2019-01-28 18:41:14 +0300

    First Commit, Upload

---
+++ aboutus1.php
@@ -0,0 +1,126 @@
+<?php
+	include("function/session.php");
+	include("db/dbconn.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MJWedding</title>
+	<link rel="icon" href="img/logo.jpg" />
+	<link rel = "stylesheet" type = "text/css" href="css/style.css" media="all">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<script src="js/bootstrap.js"></script>
+	<script src="js/jquery-1.7.2.min.js"></script>
+	<script src="js/carousel.js"></script>
+	<script src="js/button.js"></script>
+	<script src="js/dropdown.js"></script>
+	<script src="js/tab.js"></script>
+	<script src="js/tooltip.js"></script>
+	<script src="js/popover.js"></script>
+	<script src="js/collapse.js"></script>
+	<script src="js/modal.js"></script>
+	<script src="js/scrollspy.js"></script>
+	<script src="js/alert.js"></script>
+	<script src="js/transition.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div id="header">
+		<img src="img/logo2.jpg">
+		<label>MJWedding</label>
+
+			<?php
+				$id = (int) $_SESSION['id'];
+
+					$query = mysqli_query ($conn, "SELECT * FROM customer WHERE customerid = '$id' ") or die (mysqli_error());
+					$fetch = mysqli_fetch_array ($query);
+			?>
+
+			<ul>
+				<li><a href="function/logout.php"><i class="icon-off icon-white"></i>logout</a></li>
+				<li>Welcome:&nbsp;&nbsp;&nbsp;<a href="#profile" href  data-toggle="modal"><i class="icon-user icon-white"></i><?php echo $fetch['firstname']; ?>&nbsp;<?php echo $fetch['lastname'];?></a></li>
+			</ul>
+	</div>
+
+	<div id="profile" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="width:700px;">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+					<h3 id="myModalLabel">My Account</h3>
+				</div>
+					<div class="modal-body">
+						<?php
+							$id = (int) $_SESSION['id'];
+
+								$query = mysqli_query ($conn, "SELECT * FROM customer WHERE customerid = '$id' ") or die (mysqli_error());
+								$fetch = mysqli_fetch_array ($query);
+						?>
+						<center>
+					<form method="post">
+						<center>
+							<table>
+								<tr>
+									<td class="profile">Name:</td><td class="profile"><?php echo $fetch['firstname'];?>&nbsp;<?php echo $fetch['mi'];?>&nbsp;<?php echo $fetch['lastname'];?></td>
+								</tr>
+								<tr>
+									<td class="profile">Address:</td><td class="profile"><?php echo $fetch['address'];?></td>
+								</tr>
+								<tr>
+									<td class="profile">Country:</td><td class="profile"><?php echo $fetch['country'];?></td>
+								</tr>
+								<tr>
+									<td class="profile">ZIP Code:</td><td class="profile"><?php echo $fetch['zipcode'];?></td>
+								</tr>
+								<tr>
+									<td class="profile">Mobile Number:</td><td class="profile"><?php echo $fetch['mobile'];?></td>
+								</tr>
+								<tr>
+									<td class="profile">Telephone Number:</td><td class="profile"><?php echo $fetch['telephone'];?></td>
+								</tr>
+								<tr>
+									<td class="profile">Email:</td><td class="profile"><?php echo $fetch['email'];?></td>
+								</tr>
+							</table>
+						</center>
+					</div>
+				<div class="modal-footer">
+					<a href="account.php?id=<?php echo $fetch['customerid']; ?>"><input type="button" class="btn btn-success" name="edit" value="Edit Account"></a>
+					<button class="btn btn-danger" data-dismiss="modal" aria-hidden="true">Close</button>
+				</div>
+					</form>
+			</div>
+
+
+
+	<br>
+<div id="container">
+	<div class="nav">
+			 <ul>
+				<li><a href="home.php">   <i class="icon-home"></i>Home</a></li>
+				<li><a href="dresses1.php"> 			 <i class="icon-th-list"></i>Product</a></li>
+				<li><a href="aboutus1.php">   <i class="icon-bookmark"></i>About Us</a></li>
+				<li><a href="contactus1.php"><i class="icon-inbox"></i>Contact Us</a></li>
+
+			</ul>
+	</div>
+	<img src="img/banner.png" style="width:1150px; height:360px; border:1px solid #000; ">
+	<br />
+	<br />
+
+	<h1>We are happy to serve you, at MJWedding we want to make your day amazing.We offer a wide range of styles and sizes at reasonable prices so you can have the celebration of your dreams. And we stock more than just wedding dresses – there’s shoes, hair accessories, jewellery, and party dresses, too.
+
+</h1>
+
+
+		</div>
+	<br />
+</div>
+	<br />
+	<div id="footer">
+		<div class="foot">
+			<p style="font-size:25px;">&copy; 2019 MJWedding</p>
+		</div>
+
+
+	</div>
+</body>
+</html>
---
+++ db/dbconn.php
@@ -0,0 +1,3 @@
+<?php
+	$conn=mysqli_connect("localhost","root","Tanzania@1", "mjbrides") or die(mysql_error());
+?>
